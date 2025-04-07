a875a3591ad119730259ece75bb7968ffe195499
Fademacios <122602505+Fademacios@users.noreply.github.com>
2025-04-07 12:00:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,4 +9,5 @@ function changeColor(kolor){
 btn.addEventListener('click', function(){
     changeColor(changeColorInp.value)
     this.style.backgroundColor = '#ff0'
+    element.textContent = "new_value"
 })
